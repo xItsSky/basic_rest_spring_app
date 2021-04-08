f85711a26c3230db20f0d67eb928c2a67e958be6
quentin <emo.quentin@gmail.com>
2021-04-08 17:09:48 +0200

    First Commit

---
+++ src/test/java/com/itssky/springrestapp/SpringRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itssky.springrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
